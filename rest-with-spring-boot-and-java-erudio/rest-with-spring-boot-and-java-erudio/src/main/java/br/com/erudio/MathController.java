47eadd714a037d259abff07ad2089021870b378b
gemeoslemes <victorlemes0776@gmail.com>
2024-04-29 11:58:09 -0300

    Criando o Controller para trabalhar com a anotação @PathVariable

---
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,18 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double greeting(@PathVariable(value = "numberOne") String numberOne,
+                           @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
+    }
+}
