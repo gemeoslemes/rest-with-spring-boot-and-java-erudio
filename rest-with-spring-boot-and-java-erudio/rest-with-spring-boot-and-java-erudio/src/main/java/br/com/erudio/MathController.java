ca14819aaeffc73e1f5e9ea4bbd4efe9bf240f94
gemeoslemes <victorlemes0776@gmail.com>
2024-04-29 13:07:26 -0300

    Criando métodos para executar operações matemáticas e coverter  paramêtros string para double

--- rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -12,7 +12,23 @@ public class MathController {
 
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double greeting(@PathVariable(value = "numberOne") String numberOne,
-                           @PathVariable(value = "numberTwo") String numberTwo) {
-        return 1D;
+                           @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+    }
+
+    private Double covertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
