2d97ea673e46cca52f1a5b46415e1a9eca57bd50
gemeoslemes <victorlemes0776@gmail.com>
2024-05-02 16:28:46 -0300

    Criando metodo findAll na classe PersonService e no Controler para buscar todas as pessoas

--- rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonService.java
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonService.java
@@ -3,6 +3,8 @@ package br.com.erudio.services;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,7 +13,15 @@ public class PersonService {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
-
+    public List<Person> findAll() {
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
 
     public Person findById(String id) {
         logger.info("Finding one person!");
@@ -21,6 +31,16 @@ public class PersonService {
         person.setFirstName("Victor");
         person.setLastName("Lemes");
         person.setAddress("São Paulo - São Paulo - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress(" São Paulo - Brasil " + i);
         person.setGender("Male");
         return person;
     }
