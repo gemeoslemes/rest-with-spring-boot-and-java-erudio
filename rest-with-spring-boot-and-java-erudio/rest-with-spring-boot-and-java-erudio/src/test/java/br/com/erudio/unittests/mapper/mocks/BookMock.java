aa9a5869b6ceaaaee965af2684f4b7c99687779e
gemeoslemes <victorlemes0776@gmail.com>
2024-05-13 15:45:10 -0300

    Criando Mock de Book e BookVO

---
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mapper/mocks/BookMock.java
@@ -0,0 +1,54 @@
+package br.com.erudio.unittests.mapper.mocks;
+
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.model.book.Book;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class BookMock {
+    public Book mockEntity() {
+        return mockEntity(0);
+    }
+
+    public BookVO mockVO() {
+        return mockVO(0);
+    }
+
+    public List<BookVO> mockListVO() {
+        List<BookVO> bookVOList = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            bookVOList.add(mockVO(i));
+        }
+        return bookVOList;
+    }
+
+    public List<Book> mockListEntity() {
+        List<Book> bookList = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            bookList.add(mockEntity(i));
+        }
+    }
+
+    public Book mockEntity(Integer number) {
+        Book book = new Book();
+        book.setId(number.longValue());
+        book.setTitle("Title :" + number);
+        book.setLaunchDate(new Date());
+        book.setAuthor("Name Author: " + number);
+        book.setPrice(number.doubleValue());
+        return book;
+    }
+
+    public BookVO mockVO(Integer number) {
+        BookVO book = new BookVO();
+        book.setKey(number.longValue());
+        book.setTitle("Title :" + number);
+        book.setLaunchDate(new Date());
+        book.setAuthor("Name Author: " + number);
+        book.setPrice(number.doubleValue());
+        return book;
+    }
+
+}
