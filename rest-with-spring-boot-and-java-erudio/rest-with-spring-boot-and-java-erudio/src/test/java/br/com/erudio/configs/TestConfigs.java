bc592cb335f21a0393dc37bb03ee15f1ba7e7e60
gemeoslemes <victorlemes0776@gmail.com>
2024-05-15 15:49:01 -0300

    Criando toda a configuração e o ambiente de teste

--- rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -1,2 +1,6 @@
-package br.com.erudio.configs;public class TestConfigs {
+package br.com.erudio.configs;
+
+public class TestConfigs {
+
+    public static final int SERVER_PORT = 8888;
 }
--- rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest.java
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -1,2 +1,28 @@
-package br.com.erudio.integrationtests.swagger;public class SwaggerIntegrationTest {
+package br.com.erudio.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+
+import br.com.erudio.configs.TestConfigs;
+import br.com.erudio.integrationtests.AbstraticIntegrationTest;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstraticIntegrationTest {
+
+    @Test
+    public void shouldDisplaySwaggerUiPage() {
+        var content =  given()
+                .basePath("/swagger-ui/index.html")
+                .port(TestConfigs.SERVER_PORT)
+                .when()
+                   .get()
+                .then()
+                   .statusCode(200)
+                .extract()
+                    .body()
+                        .asString();
+        assertTrue(content.contains("Swagger UI"));
+    }
 }
